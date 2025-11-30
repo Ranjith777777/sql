f9af8fdb31d1eba41cb3f196adb9ffe395624528
Ranjith777777 <ranjithsanjay01@gmail.com>
2025-11-30 17:52:38 +0530

    first commit

---
+++ Dml.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE CLASS;
+USE CLASS;
+CREATE TABLE table1(
+ id INT PRIMARY KEY auto_increment,
+ name varchar(20),
+ age int,
+city varchar(30),
+email varchar(40),
+mobile varchar(10)
+
+);
+ INSERT INTO table1
+
+
+
+
+
+); 
