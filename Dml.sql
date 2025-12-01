186fddaf6c2fe2e8dd8e78659028fc30693e5017
Ranjith777777 <ranjithsanjay01@gmail.com>
2025-12-01 10:51:17 +0530

    updated

--- Dml.sql
+++ Dml.sql
@@ -1,18 +1,15 @@
 CREATE DATABASE CLASS;
 USE CLASS;
 CREATE TABLE table1(
+
  id INT PRIMARY KEY auto_increment,
  name varchar(20),
  age int,
-city varchar(30),
-email varchar(40),
-mobile varchar(10)
+ city varchar(30),
+ email varchar(40),
+ mobile varchar(10));
+INSERT INTO table1 ( name ,age,city,email,mobile )values("ranjith",20,"pondicherry","ranjith@gmail.com","9361780926");
+ 
+describe table1;
 
-);
- INSERT INTO table1
-
-
-
-
-
-); 
+select * from table1;
