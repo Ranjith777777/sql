f1516ea9e808c88a2d2aec2302b10f0d707b90d4
Ranjith777777 <ranjithsanjay01@gmail.com>
2025-12-01 11:02:10 +0530

    updated

--- Dml.sql
+++ Dml.sql
@@ -9,7 +9,9 @@ CREATE TABLE table1(
  email varchar(40),
  mobile varchar(10));
 INSERT INTO table1 ( name ,age,city,email,mobile )values("ranjith",20,"pondicherry","ranjith@gmail.com","9361780926");
- 
+insert into table1(name,age,city,email,mobile)value("yuva",24,"chennai","yuva@gmail.com","8967564567");
+insert into table1(name,age,city,email,mobile)value("dhoni",42,"ranchi","dhoni@gmail.com","7896775678");
+insert into table1(name,age,city,email,mobile)value("rohit",36,"mumbai","rohit@gamil.com","8976743567");
 describe table1;
 
 select * from table1;
